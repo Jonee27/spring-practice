d091612affcd16e627ffcd141213ed381913f6ec
Jonee27 <66450919+Jonee27@users.noreply.github.com>
2021-03-30 00:57:50 +0200

    main prints out ApplicationContext's beans

--- src/main/java/com/example/springpractice/SpringpracticeApplication.java
+++ src/main/java/com/example/springpractice/SpringpracticeApplication.java
@@ -1,13 +1,35 @@
 package com.example.springpractice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+
+
+//Kiváltja a fenti 3 annotációt
+//@ComponentScan({"com.example.springpractice", "com.example.springpractice.spy"})
 @SpringBootApplication
 public class SpringpracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringpracticeApplication.class, args);
+        ApplicationContext ct = SpringApplication.run(SpringpracticeApplication.class, args);
+
+        String[] beanArray = ct.getBeanDefinitionNames();
+        Arrays.sort(beanArray);
+
+        for (String name: beanArray){
+            System.out.println(name);
+        }
     }
 
 }
+
